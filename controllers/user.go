2c1d114d32ed34105a12d6c2e41c81146e1c54e3
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-02-27 12:56:13 +0700

    [User controller ] register user

---
+++ controllers/user.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"bwa-startup/helpers"
+	"bwa-startup/users"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type userController struct {
+	userService users.Service
+}
+
+func NewUserController (userService users.Service) *userController{
+	return &userController{userService}
+}
+
+func (h *userController) RegisterUser(c *gin.Context){
+	//Tangkap input dari user
+	//Map input user ke struct RegisterUserInput
+	//Struct diatas di pasrsing sebagai parameter service
+
+	var input users.RegisterUserInput
+	err := c.BindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	response := helpers.APIResponse("Account has ben registered", http.StatusOK, "success", user)
+	c.JSON(http.StatusOK, response)
+
+}
