4fcca49c2620702e66f3b1eac246ab41f430ef9f
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-02-28 10:55:42 +0700

    get list campaigs - controller

---
+++ controllers/campaign.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"bwa-startup/campaign"
+	"bwa-startup/helpers"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type campaignController struct {
+	service campaign.Service
+}
+
+func NewCampaignController(service campaign.Service) *campaignController {
+	return &campaignController{service}
+}
+
+func (h *campaignController) GetCampaigns(c *gin.Context) {
+	//mengubah string parameter user_id ke int, dgn return nilai, error
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helpers.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpers.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
