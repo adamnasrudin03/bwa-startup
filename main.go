a948ff7ae6c89269dbf6e024eaafb9c29d4daeda
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-01 13:44:45 +0700

    endpoint transaction notification

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ func main() {
 	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionController.GetCampaignTransaction)
 	api.GET("/transactions", authMiddleware(authService, userService), transactionController.GetUserTransaction)
 	api.POST("/transactions", authMiddleware(authService, userService), transactionController.CreateTransaction)
+	api.POST("/transactions/notification", transactionController.GetNotification)
 
 	router.Run()
 
