2f16982a67168aae32dc6ba3caeed3d2d3b68b52
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-01 09:46:06 +0700

    CreateTransactio - input

--- transaction/input.go
+++ transaction/input.go
@@ -6,3 +6,9 @@ type GetTransactionByCampaignIdInput struct {
 	ID   int `uri:"id" binding:"required"`
 	User users.User
 }
+
+type CreateTransactionInput struct {
+	Amount   	int `uri:"amount" binding:"required"`
+	CampaignID  int `uri:"campaign_id" binding:"required"`
+	User 		users.User
+}
