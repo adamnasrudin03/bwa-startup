dd575c4ebc28e22127f01614608e6cf68909a0d2
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-01 07:44:41 +0700

    transaction - init controller

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -1,1 +1,11 @@
 package controllers
+
+import "bwa-startup/transaction"
+
+type transactionController struct {
+	service transaction.Service
+}
+
+func NewTransactionController(service transaction.Service) *transactionController {
+	return &transactionController{service}
+}
