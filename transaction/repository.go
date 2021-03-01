584949d86549bb916de958495050d5b47b63e36c
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-01 09:02:58 +0700

    list transaction by userid - repository

--- transaction/repository.go
+++ transaction/repository.go
@@ -7,6 +7,7 @@ type repository struct {
 }
 type Repository interface {
 	GetByCampaignId(campaignID int) ( []Transaction, error )
+	GetByUserId(userID int) ([]Transaction, error)
 
 }
 
@@ -17,6 +18,18 @@ func NewRepository(db *gorm.DB) *repository {
 func (r *repository) GetByCampaignId(campaignID int) ( []Transaction, error ) {
 	var transactions []Transaction
 	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (r *repository) GetByUserId(userID int) ([]Transaction, error) {
+	var transactions []Transaction
+	//preload [load data realasi ke campaign lalu ke campaign images, yg di load
+	// di campaign images is_primary == 1]
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary == 1").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
 	if err != nil {
 		return transactions, err
 	}
