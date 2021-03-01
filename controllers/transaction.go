c84cf50b09730c634c933b497d003939dbae37ae
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-01 07:50:16 +0700

    get transaction By campignId - controller

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "bwa-startup/transaction"
+import (
+	"bwa-startup/helpers"
+	"bwa-startup/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 
 type transactionController struct {
 	service transaction.Service
@@ -8,4 +15,25 @@ type transactionController struct {
 
 func NewTransactionController(service transaction.Service) *transactionController {
 	return &transactionController{service}
+}
+
+func (h *transactionController) GetCampaignTransaction (c *gin.Context) {
+	var input transaction.GetTransactionByCampaignIdInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helpers.APIResponse("Failed to get campaign's transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionByCampaignId(input)
+	if err != nil {
+		response := helpers.APIResponse("Failed to get campaign's transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpers.APIResponse("Campaign's transaction", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
 }
