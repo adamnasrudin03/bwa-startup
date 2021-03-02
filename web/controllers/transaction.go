8ce1962abb509356dc77ae5bdb59b7683645a738
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-02 13:50:39 +0700

    web cms - transaction get all controller

---
+++ web/controllers/transaction.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"bwa-startup/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type transactionController struct {
+	transactionService transaction.Service
+}
+
+func NewtransactionController(transactionService transaction.Service) *transactionController {
+	return &transactionController{transactionService}
+}
+
+func (h *transactionController) Index(c *gin.Context) {
+	transactions, err := h.transactionService.GetAllTransaction()
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	c.HTML(http.StatusOK, "transaction_index.html", gin.H{"transactions": transactions})
+}
