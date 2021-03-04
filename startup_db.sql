7b3baadc132f7d30fbd6b6ac7701dc14de915751
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-04 11:31:19 +0700

    cange export db

--- startup_db.sql
+++ startup_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2021 at 05:03 AM
+-- Generation Time: Mar 04, 2021 at 05:29 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -48,7 +48,7 @@ CREATE TABLE `campaigns` (
 
 INSERT INTO `campaigns` (`id`, `user_id`, `name`, `short_description`, `description`, `perks`, `backer_count`, `goal_amount`, `current_amount`, `slug`, `created_at`, `updated_at`) VALUES
 (1, 2, 'Campaign punya Adam nasrudin', 'Cras faucibus magna vel blandit ultrices. Integer vitae aliquam nisl.', '            Cras faucibus magna vel blandit ultrices. Integer vitae aliquam nisl. Duis ullamcorper gravida tortor eget ornare. Aenean ante eros, dignissim nec dictum dapibus, auctor non elit. Fusce hendrerit est eu lectus gravida, eu posuere lectus lobortis. Quisque hendrerit odio et blandit consequat. Donec rutrum risus sit amet eros lacinia hendrerit. Curabitur congue dui sapien, et consectetur augue gravida at. Praesent erat tortor, venenatis at augue sit amet, ullamcorper mattis libero. Morbi feugiat pellentesque mauris et semper. Proin interdum tellus sit amet lorem tristique porttitor. Vivamus dapibus, eros vulputate euismod ultricies, tellus quam blandit enim, id porta augue augue et velit. Nam at efficitur est.\r\n\r\nDonec dapibus scelerisque accumsan. In hac habitasse platea dictumst. Proin et sapien sit amet ante vehicula vestibulum. Vivamus congue, tellus ut gravida consectetur, ligula felis tincidunt diam, sit amet placerat nulla nunc nec justo. Curabitur sit amet nibh at augue vestibulum accumsan vel eget massa. Cras eget sapien vel felis tempus eleifend non in eros. Curabitur id efficitur ex, eu volutpat sapien. Integer interdum sollicitudin velit, eget vehicula tortor fringilla sed. Nulla condimentum imperdiet massa ut interdum. Integer at ipsum sed odio lobortis dignissim. Morbi ac lectus suscipit, finibus ipsum sit amet, dapibus eros.\r\n        ', 'kecepatan yang ditawarkannya, tidak akan mengizinkan penggunanya untuk menghamburkan variabel atau data-data impor yang tidak digunakan, memiliki compiler yang dapat digunakan untuk mengkompilasi proyek dengan sangat cepat', 0, 200000, 0, 'campaign-punya-user-1', '2021-03-02 08:59:25', '2021-03-04 11:02:27'),
-(2, 3, 'project name', 'project ini adalah', '                                    \r\n        PROJECT by fahriPROJECT by fahriPROJECT by fahriPROJECT by fahri PROJECT by fahri\r\n        \r\n        ', 'Keuntungan 1, Keuntungan 2, Keuntungan 3, Keuntungan 4', 0, 500000, 0, 'project-by-fahri-16', '2021-03-02 09:44:21', '2021-03-04 11:03:19');
+(2, 3, 'project name', 'project ini adalah', '                                    \r\n        PROJECT by fahriPROJECT by fahriPROJECT by fahriPROJECT by fahri PROJECT by fahri\r\n        \r\n        ', 'Keuntungan 1, Keuntungan 2, Keuntungan 3, Keuntungan 4', 1, 500000, 350000, 'project-by-fahri-16', '2021-03-02 09:44:21', '2021-03-04 11:27:24');
 
 -- --------------------------------------------------------
 
@@ -95,7 +95,9 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `user_id`, `campaign_id`, `amount`, `status`, `code`, `payment_url`, `created_at`, `updated_at`) VALUES
-(15, 2, 1, 12345678, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/f1dd2953-c584-4b12-8e98-cf1c0b344329', '2021-03-02 13:56:19', '2021-03-02 13:56:21');
+(15, 2, 1, 12345678, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/f1dd2953-c584-4b12-8e98-cf1c0b344329', '2021-03-02 13:56:19', '2021-03-02 13:56:21'),
+(16, 2, 2, 200000, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/5bdafdb7-6721-4f14-a9b8-40e469b5b0cb', '2021-03-04 11:12:32', '2021-03-04 11:12:34'),
+(18, 2, 2, 350000, 'paid', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/4d433793-d2b1-4c6e-bba4-6c23b410e157', '2021-03-04 11:25:00', '2021-03-04 11:27:24');
 
 -- --------------------------------------------------------
 
@@ -172,7 +174,7 @@ ALTER TABLE `campaign_images`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
